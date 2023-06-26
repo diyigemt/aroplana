1d93a9c2b7f28d104f81fcc2fb3226115877451d
diyigemt <qwe1355247243@gmail.com>
2023-06-26 23:20:56 +0800

    init(webui): init

---
+++ arona-webui/src/layouts/Footer/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineOptions({
+  name: "LayoutFooter",
+});
+</script>
+
+<template>
+  <footer
+    class="footer footer-center sticky bottom-0 border-t border-base-100 bg-base-200 px-4 pt-2 text-base-content opacity-90"
+  />
+</template>
---
+++ arona-webui/src/main.ts
@@ -0,0 +1,36 @@
+import { useLocalStorage } from "@vueuse/core";
+import { createApp } from "vue";
+
+// Vue Router
+import { createPinia } from "pinia";
+import { createI18n } from "vue-i18n";
+
+import { router } from "./router";
+
+import { registerStore } from "./store";
+import App from "~/App.vue";
+
+// reset css
+import "@kirklin/reset-css/kirklin.css";
+import "~/styles/main.css";
+import "uno.css";
+
+import "element-plus/dist/index.css";
+
+const app = createApp(App);
+const messages = Object.fromEntries(
+  Object.entries(
+    import.meta.glob<{ default: any }>("./locales/*.json", { eager: true }))
+    .map(([key, value]) => {
+      return [key.slice(10, -5), value.default];
+    }),
+);
+app.use(createI18n({
+  legacy: false,
+  locale: unref(useLocalStorage("locale", "zh-CN")),
+  messages,
+}));
+app.use(createPinia());
+registerStore();
+app.use(router);
+app.mount("#app");
